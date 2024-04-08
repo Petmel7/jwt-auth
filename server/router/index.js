454cb24a65f7819734488def299945adfa214961
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2024-04-08 13:59:05 +0300

    add

--- server/router/index.js
+++ server/router/index.js
@@ -11,37 +11,3 @@ router.get('/refresh', userController.refresh);
 router.get('/users', userController.getUsers);
 
 module.exports = router
-
-
-
-
-
-// const Router = require('express').Router;
-
-// const router = new Router();
-
-// router.post('/registration', (req, res) => {
-//     // Обробка запиту реєстрації
-// });
-
-// router.post('/login', (req, res) => {
-//     // Обробка запиту входу
-// });
-
-// router.post('/logout', (req, res) => {
-//     // Обробка запиту виходу
-// });
-
-// router.get('/activate/:link', (req, res) => {
-//     // Обробка запиту активації
-// });
-
-// router.get('/refresh', (req, res) => {
-//     // Обробка запиту оновлення
-// });
-
-// router.get('/users', (req, res) => {
-//     // Обробка запиту для отримання списку користувачів
-// });
-
-// module.exports = router;

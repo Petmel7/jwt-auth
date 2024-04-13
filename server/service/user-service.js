92d820256f42fcd97e0365d7baaa7badd63300c0
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2024-04-13 13:45:39 +0300

    add

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,42 +1,13 @@
 
-// const UserModel = require("../models/user-model");
-// const bcrypt = require('bcrypt');
-// const uuid = require('uuid');
-// const mailService = require('./mail-service');
-// const tokenService = require('./token-service');
-// const UserDto = require('../dtos/user-dto');
+const UserModel = require("../models/user-model");
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./mail-service');
+const tokenService = require('./token-service');
+const UserDto = require('../dtos/user-dto');
 
-// class UserService {
-//     async registration(email, password) {
-//         const candidate = await UserModel.findOne({ email });
-//         if (candidate) {
-//             throw new Error(`Користувач з поштовим адресом ${email} вже існує`);
-//         }
-
-//         const hashPassword = await bcrypt.hash(password, saltRounds);
-
-//         const activationLink = uuid.v4();
-
-//         const user = await UserModel.create({ email, password: hashPassword, activationLink });
-//         await mailService.mailService();
-
-//         const userDto = new UserDto(user);
-//         const tokens = tokenService.generateToken({ ...userDto });
-//         await tokenService.saveToken(userDto.id, tokens.refreshToken);
-//         return { ...tokens, user: userDto };
-//     }
-// }
-
-// module.exports = new UserService();
-
-const UserModel = "../models/user-model";
-const bcrypt = 'bcrypt';
-const uuid = 'uuid';
-const mailService = './mail-service';
-const tokenService = './token-service';
-const UserDto = '../dtos/user-dto';
-
-const saltRounds = 10; // Define the number of salt rounds for bcrypt
+const saltRounds = 3;
+console.log(typeof saltRounds);
 
 class UserService {
     async registration(email, password) {
@@ -47,18 +18,50 @@ class UserService {
 
         const hashPassword = await bcrypt.hash(password, saltRounds);
 
-        const activationLink = uuid.v4(); // Generate a unique activation link
+        const activationLink = uuid.v4();
 
         const user = await UserModel.create({ email, password: hashPassword, activationLink });
-        await mailService.sendActivationMail(email, activationLink); // Corrected method call
+        await mailService.sendActivationMail(email, activationLink);
 
-        const userDto = new UserDto(user); // Create a new UserDto
+        const userDto = new UserDto(user);
         const tokens = tokenService.generateToken({ ...userDto });
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
-        return { ...tokens, user: userDto }; // Return the tokens and user data
+        return { ...tokens, user: userDto };
     }
 }
 
 module.exports = new UserService();
+
+
+
+// const UserModel = require("../models/user-model");
+// const bcrypt = require('bcrypt');
+// const uuid = require('uuid');
+// const mailService = require('./mail-service');
+// const tokenService = require('./token-service');
+// const UserDto = require('../dtos/user-dto');
+
+// const saltRounds = 3;
+// console.log(typeof saltRounds);
+
+// class UserService {
+//     async registration(email, password) {
+//         const candidate = await UserModel.findOne({ email });
+//         if (candidate) {
+//             throw new Error(`Користувач з поштовим адресом ${email} вже існує`);
+//         }
+
+//         const user = await UserModel.create({ email, password: password });
+//         await mailService.sendActivationMail(email);
+
+//         const userDto = new UserDto(user);
+//         const tokens = tokenService.generateToken({ ...userDto });
+//         await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+//         return { ...tokens, user: userDto };
+//     }
+// }
+
+// module.exports = new UserService();
 
